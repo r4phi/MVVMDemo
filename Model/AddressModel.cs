c4a820335f1571de6f570551a50d736aafe3ba8f
raphi <raphi.kapeller@gmx.at>
2021-10-22 11:52:06 +0200

    ende der stunde

--- Model/AddressModel.cs
+++ Model/AddressModel.cs
@@ -1,16 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace MVVM.Model
 {
-    class AddressModel
+    class AddressModel : INotifyPropertyChanged
     {
-        public string Name { get; set; } = string.Empty;
-        public string Street { get; set; } = string.Empty;
-        public int CipCode { get; set; }
-        public string City { get; set; } = string.Empty;
+        #region properties
+        private string name = string.Empty;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                RaisePropertyChanged(); //Refresh View
+            }
+        }
+        private string street = string.Empty;
+        public string Street
+        {
+            get => street;
+            set
+            {
+                street = value;
+                RaisePropertyChanged();
+            }
+        }
+        private int cipCode;
+        public int CipCode
+        {
+            get => cipCode;
+            set
+            {
+                cipCode = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string city = string.Empty;
+        public string City
+        {
+            get => city;
+            set
+            {
+                city = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string address = string.Empty;
+        public string Address
+        {
+            get => address;
+            set
+            {
+                address = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region INotify
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region methods
+        public void Reset()
+        {
+            Name = Street = City = Address = String.Empty;
+            CipCode = 0;
+        }
+
+        public void BuildAddress()
+        {
+            Address = $"{Name}{Environment.NewLine}{Street}{Environment.NewLine}{CipCode} {Street}";
+        }
+        #endregion
     }
 }
--- ViewModel/DemoView.cs
+++ ViewModel/DemoView.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MVVM.Model;
+﻿using MVVM.Model;
+using System.Windows.Input;
 
 namespace VMMVDemo.ViewModel
 {
-	class DemoView
-	{
+    class DemoView
+    {
+        #region Properties
         public AddressModel Address { get; set; } = new AddressModel();
-	}
+        #endregion
+
+        #region methods
+        public DemoView()
+        {
+            DeleteCommand = new RelayCommand(e =>
+            {
+                Address.Reset();
+            }, c => true);
+
+            AddressCommand = new RelayCommand(e =>
+            {
+                Address.BuildAddress();
+            }, c => Address.Name != string.Empty);
+        }
+        #endregion
+
+        #region commands
+        public ICommand DeleteCommand { get; private set; }
+        public ICommand AddressCommand { get; private set; }
+        #endregion
+    }
 }
