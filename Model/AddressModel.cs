8e7bce67253ff25909f297a4e768dd9d4de827c3
raphi <raphi.kapeller@gmx.at>
2021-10-22 10:46:15 +0200

    Projektdateien hinzufügen.

---
+++ Model/AddressModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Model
+{
+    class AddressModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Street { get; set; } = string.Empty;
+        public int CipCode { get; set; }
+        public string City { get; set; } = string.Empty;
+    }
+}
